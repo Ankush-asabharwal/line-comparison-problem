7f43ee605d198f047a4cc290bfa01c2bd070982d
Ankush-asabharwal <ankusha0209.be21@gmail.com>
2024-12-26 10:10:43 +0530

    [Ankusha] add : UC3 is complete and now adding to git

--- line-comparison.cpp
+++ line-comparison.cpp
@@ -8,11 +8,20 @@ int length(int x1, int x2, int y1, int y2){
     return sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 }
 
-bool isLineEqual(int x1a, int x2a, int y1a, int y2a,
+void comparingLines(int x1a, int x2a, int y1a, int y2a,
                  int x1b, int x2b, int y1b, int y2b){
                     int len1 = length(x1a,x2a,y1a,y2a);
                     int len2 = length(x1b,x2b,y1b,y2b);
-                    return len1-len2;
+                    cout << "Length of Linea is : " << len1 << endl;
+                    cout << "Length of Lineb is : " << len2 << endl;
+                    if(len1<len2){
+                        cout<< "Line a is smaller than line b"<< endl;
+                    }
+                    else if(len1>len2){
+                        cout<< "Line b is smaller than line a"<< endl;
+                    }else{
+                        cout<< "Line a and line b is equal"<< endl;
+                    }
                 }
 
 int main(){
@@ -24,12 +33,7 @@ int main(){
     cout << "Enter coordinates for Line 2 (x1, y1, x2, y2): ";
     int x1b, y1b, x2b, y2b;
     cin >> x1b >> y1b >> x2b >> y2b;
-    if(isLineEqual(x1a, y1a, x2a, y2a, x1b, y1b, x2b, y2b)){
-        cout << "The lines are equal" << endl;
-    }
-    else {
-        cout << "The lines are not equal." << endl;
-    }
+    comparingLines(x1a, y1a, x2a, y2a, x1b, y1b, x2b, y2b);
 
     return 0;
 }
